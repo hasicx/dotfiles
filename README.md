80cfce649863b8632ed70d2e33898d4074f2a7e4
Adel Hasic <hasic@student.chalmers.se>
2020-08-04 15:06:22 +0200

    update 4/8-20

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # dotfiles
+Run symlink.sh to generate soft links (symlinks) in $HOME (~) to .../dotfiles/.* (i.e. to the absolute paths of the dot-files in dotfiles/)
---
+++ symlink.sh
@@ -0,0 +1,10 @@
+VIMFILE=".vimnewrc"
+
+ln -sf /home/hasic_/dotfiles/.bashrc /home/hasic_/ # .bashrc
+ln -sf /home/hasic_/dotfiles/.dircolors /home/hasic_/ # .dircolors (nord-dircolors)
+ln -sf /home/hasic_/dotfiles/.gitconfig /home/hasic_/ # .gitconfig
+ln -sf /home/hasic_/dotfiles/.profile /home/hasic_/ # .tmux.conf
+ln -sf /home/hasic_/dotfiles/.tmux /home/hasic_/ # .tmux/
+ln -sf /home/hasic_/dotfiles/.tmux.conf /home/hasic_/ # .tmux.conf
+ln -sf /home/hasic_/dotfiles/.vim /home/hasic_/ # .vim/
+ln -sf /home/hasic_/dotfiles/$VIMFILE /home/hasic_/ # .vimrc
